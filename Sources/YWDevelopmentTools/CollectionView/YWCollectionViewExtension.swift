c24d6073de0b1f5c21dcd95585fd979230b23ddb
youwei <youv686@gmail.com>
2023-06-21 16:20:57 +0800

    collection view 改为公共的拓展

--- Sources/YWDevelopmentTools/CollectionView/YWCollectionViewExtension.swift
+++ Sources/YWDevelopmentTools/CollectionView/YWCollectionViewExtension.swift
@@ -1,49 +1,28 @@
 import UIKit
 
-public struct CollectionViewContainer {
-    
-    public let collectionView: UICollectionView
-    public init(collectionView: UICollectionView) {
-        self.collectionView = collectionView
+public extension UICollectionView {
+
+    func register<T: YWXibCollectionViewCell>(_: T.Type) where T: YWNibLoadable {
+        register(T.nib, forCellWithReuseIdentifier: T.identifier)
     }
     
-    public func register<T: YWXibCollectionViewCell>(_: T.Type) where T: YWNibLoadable {
-        collectionView.register(T.nib, forCellWithReuseIdentifier: T.identifier)
-    }
-    
-    public func dequeueReusableCell<T: YWXibCollectionViewCell>(for indexPath: IndexPath) -> T {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: T.identifier, for: indexPath) as? T
+    func dequeueReusableCell<T: YWXibCollectionViewCell>(for indexPath: IndexPath) -> T {
+        let cell = dequeueReusableCell(withReuseIdentifier: T.identifier, for: indexPath) as? T
         guard let cell = cell else {
             fatalError("The dequeueReusableCell \(String(describing: T.self)) couldn't be loaded.")
         }
         return cell
     }
     
-    public func register<T: YWClassCollectionViewCell>(_: T.Type) where T: YWReusable {
-        collectionView.register(T.self, forCellWithReuseIdentifier: T.identifier)
+    func register<T: YWClassCollectionViewCell>(_: T.Type) where T: YWReusable {
+        register(T.self, forCellWithReuseIdentifier: T.identifier)
     }
     
-    public func dequeueReusableCell<T: YWClassCollectionViewCell>(for indexPath: IndexPath) -> T {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: T.identifier, for: indexPath) as? T
+    func dequeueReusableCell<T: YWClassCollectionViewCell>(for indexPath: IndexPath) -> T {
+        let cell = dequeueReusableCell(withReuseIdentifier: T.identifier, for: indexPath) as? T
         guard let cell = cell else {
             fatalError("The dequeueReusableCell \(String(describing: T.self)) couldn't be loaded.")
         }
         return cell
-    }
-}
-
-
-extension UICollectionView {
-    /// 使用yw调用开发工具的方法。
-    ///
-    /// 例子
-    /// ```
-    /// let collectionView = UICollectionView()
-    /// collection.yw.register()
-    /// ...
-    ///
-    /// ```
-    var yw: CollectionViewContainer {
-        CollectionViewContainer(collectionView: self)
     }
 }
